98a88464f37a1b613b5b1623b9bb141533b7678a
Etem Keskin <etmkeskin@gmail.com>
2022-09-28 21:07:43 -0400

    overlapping conditionals

---
+++ Prototype_3/src/concole_apps/GreatApp2.java
@@ -0,0 +1,24 @@
+package concole_apps;
+
+import java.util.Scanner;
+
+import model.Grade;
+
+public class GreatApp2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("Enter a raw mark score (from 0 to 100): ");
+		int marks = input.nextInt();
+		String lg = Grade.getLetterGrade2(marks);
+		
+		System.out.println("It is letter grade " + lg);
+		
+		
+		input.close();
+
+	}
+
+}
