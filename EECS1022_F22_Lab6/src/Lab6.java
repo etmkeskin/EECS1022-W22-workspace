98f57dbf6909828bc5a719427036e2a9e2f5c3e1
Etem Keskin <etmkeskin@gmail.com>
2022-11-13 22:03:24 -0500

    lab6 done

--- EECS1022_F22_Lab6/src/Lab6.java
+++ EECS1022_F22_Lab6/src/Lab6.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Lab6 {
 
@@ -92,19 +93,103 @@ public class Lab6 {
 		String result = "";
 		
 		int len = array.length;
-		int count = 0;
+		int[] freq = new int[len];
+		int visited = -1;
+		
 		if(len == 0) {
 			return result;
 		}
 		else {
+			//Finding frequencies of each element
 			for(int i = 0; i < len; i++) {
-				if((array[i] % 2 == 0) && (array[i] >= 2 && array[i] < 100)) {
-					for(int j = 0; j < len; j++) {
-						if(array[i] == array[j]) {
-							count += 1;
-						}
+				int count = 1;
+				for(int j = i + 1; j < len; j++) {
+					if(array[i] == array[j]) {
+						count ++;
+						freq[j] = visited;
 					}
 				}
+				if(freq[i] != visited) {
+					freq[i] = count;
+				}
+			}
+			//Finding max according to the given condition of being even and less than 100
+			int max = array[0];
+			for(int m = 0; m < len; m++) {
+				if((array[m] > max) && ((array[m] % 2 == 0)&&(array[m] < 100))) {
+					max = array[m];
+				}
+			}
+			//Creating result, using \n according to last element in freq[] array
+			for(int k = 0; k < len; k++) {
+				if((freq[k] != visited) && (array[k] % 2 == 0) && (array[k] >= 2 && array[k] < 100)) {
+					if(array[k] < max) {
+						result += array[k] + ": " + freq[k] + "\n";
+					}
+					else if(array[k] == max){
+						result += array[k] + ": " + freq[k];
+					}
+					
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	public static void swapElements (int [] array) {
+		
+		int len = array.length;
+		int evenNum = array[0];
+		if(len % 2 == 0) {
+			for(int i = 0; i < len; i += 2) {
+				for(int j = i; j < len; ) {
+					
+					evenNum = array[i + 1];
+					array[i + 1] = array[j];
+					array[j] = evenNum;
+					break;
+				}
+			}
+		}
+		else if(len % 2 == 1) {
+			for(int i = 0; i < len / 2; i++) {
+				evenNum = array[i];
+				array[i] = array[len - i - 1];
+				array[len - i - 1] = evenNum;
+				
+			}
+		}
+		
+	}
+	
+	
+	public static boolean validString (String str1, String str2) {
+		boolean result = true;
+		
+		int len1 = str1.length();
+		int len2 = str2.length();
+		char[] str1Arr = new char[len1];
+		char[] str2Arr = new char[len2];
+		char[] arrResult = new char[len2];
+		
+		for(int i = 0; i < len1; i++) {
+			str1Arr[i] = str1.charAt(i);
+		}	
+		for(int j = 0; j < len2; j++) {
+			str2Arr[j] = str2.charAt(j);
+		}
+		for(int k = 0; k < len2; k++) {
+			for(int m = k; m < len1; m++) {
+				if((str1Arr[m] == str2Arr[k]) && (str1Arr[1] == str2Arr[1])) {
+					arrResult[k] = str1Arr[m];
+				}
+			}
+		}
+		for(int i = 0; i < len2; i++) {
+			if(str2Arr[i] != arrResult[i]) {
+				result = false;
 			}
 		}
 		
@@ -114,20 +199,7 @@ public class Lab6 {
 		
 		
 		
-		
-		
-		return result + count;
-	}
-	
-	
-	public static void swapElements (int [] array) {
-		
-	}
-	
-	
-	public static boolean validString (String str1, String str2) {
-	
-			return false;
+		return result;
 	}
 	
 }
--- EECS1022_F22_Lab6/src/Lab6Console.java
+++ EECS1022_F22_Lab6/src/Lab6Console.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lab6Console {
@@ -6,19 +7,28 @@ public class Lab6Console {
 		Scanner scan = new Scanner(System.in); 	
 		
 		//longestSeq()
-//		int[] a = {7,-9,10,-11,1,-2,-1,0};
-//		int result = Lab6.longestSeq(a);
-//		System.out.println(result);
+		int[] a = {7,-9,10,-11,1,-2,-1,0};
+		int result = Lab6.longestSeq(a);
+		System.out.println(result);
 		
 		//getArrayStats()
-//		int[] a1 = {4,8,1,6,11};
-//		String result1 = Lab6.getArrayStats(a1);
-//		System.out.println(result1);
+		int[] a1 = {4,8,1,6,11};
+		String result1 = Lab6.getArrayStats(a1);
+		System.out.println(result1);
 		
 		//countArray()
-		int[] a2 = {2,10,10,98,3,146};
+		int[] a2 = {6,12,6,6,18,45};
 		String result2 = Lab6.countArray(a2);
 		System.out.println(result2);
+		
+		//swapElements()
+		
+		//validString
+		String a3 = "aedreferam";
+		String a4 = "adam";
+		boolean result3 = Lab6.validString(a3, a4);
+		System.out.println(result3);
+		
 		
 		
 		scan.close();
