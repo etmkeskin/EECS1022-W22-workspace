6b6f090b9818624a0e91ee7f49d029ecc8329ef2
Etem Keskin <etmkeskin@gmail.com>
2022-12-01 10:20:05 -0500

    lab8

---
+++ EECS1022_F22_Lab8/src/Lab8.java
@@ -0,0 +1,19 @@
+public class Lab8 {
+	
+	public static void switchPairs(ArrayList<String> list) {
+
+	}
+	public static String acronym(ArrayList<String> words) {
+
+	    return "";
+	}
+}
+class Patient {
+	
+	
+}
+
+class Physician {
+
+	
+}
