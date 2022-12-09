c8ff5638e0fd3677d3703607dbfa77539dbfe656
Etem Keskin <etmkeskin@gmail.com>
2022-12-08 22:32:29 -0500

    lab7

--- EECS1022_F22_Lab7/src/Lab7.java
+++ EECS1022_F22_Lab7/src/Lab7.java
@@ -49,35 +49,170 @@ public class Lab7 {
 		for(int i = 0; i < array.length; i++) {
 			diagonalSum1 += array[i][i];
 		}
-//		for(int i = array.length - 1; i >= 0; i--) {
-//			for(int j = 0; j < array.length; j++) {
-//				diagonalSum2 += array[i][j];
-//			}
-//		}
-		if((diagonalSum1 == diagonalSum2) && rowSum == columnSum) {
-			result = true;
+		for(int i = array.length - 1; i >= 0;) {
+			for(int j = 0; j < array.length; j++) {
+				diagonalSum2 += array[i][j];
+				i--;
+			}
+		}
+		if(diagonalSum1 == diagonalSum2) {
+			for(int i = 0; i < array.length; i++) {
+				if(rowSum[i] == columnSum[i]) {
+					result = true;
+				}
+				else {
+					return result;
+				}
+			}
 		}
 		else {
 			return result;
 		}
-		
-
-
-		
-		
-		
-		
 		return result;
 	}
 	
 }
 
-//class Patient {
-//	
-//	
-//}
-//
-//class Physician {
-//	
-//	
-//}
+class Patient {
+	private String name;
+	private int ssn;
+	private int age;
+	private char gender;
+	private String address;
+	
+	
+	public Patient() {
+		//default constructor;
+	}
+	
+	public Patient(String name, int ssn, int age, char gender, String address) {
+		this.name = name;
+		this.ssn = ssn;
+		this.age = age;
+		this.gender = gender;
+		this.address = address;
+	}
+	
+	//Accessor methods
+	public String getName() {
+		return name;
+	}
+	
+	public int getSsn() {
+		return ssn;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public char getGender() {
+		return gender;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	//Mutator methods
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Patient Name: " + name + "\t" + "SSN: " + ssn + "\t" + "Age: " + age + "\t" + 
+	"Gender: " + gender + "\t" + "Address: " + address;
+	}
+	
+}
+
+class Physician {
+	private String name;
+	private String phone;
+	private int MAX_PATIENT = 3;
+	private int pCount = 0;
+	private Patient[] patients;
+	
+	public Physician() {
+		//default constructor
+	}
+	
+	public Physician(String name, String phone) {
+		this.name = name;
+		this.phone = phone;
+		this.patients = new Patient[MAX_PATIENT];
+	}
+	
+	//Accessor Methods
+	public String getName() {
+		return name;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public int getPcount() {
+		return pCount;
+	}
+	
+	public Patient[] getPatients() {
+		return patients;
+	}
+	
+	//Mutator Methods
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public void setPatients(Patient[] patients) {
+		this.patients = patients;
+	}
+	
+	public void admitPatient(Patient patient) {
+		for(int i = pCount; i < patients.length; pCount++) {
+			if(patients.length < 3) {
+				patients[i] = patient;
+			}
+			else {
+			}
+		}
+	}
+	
+	public void increaseLength(Patient[] patients) {
+		if(pCount >= 3) {
+			MAX_PATIENT++;
+			patients = new Patient[MAX_PATIENT];
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
